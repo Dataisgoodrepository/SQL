5616d5d940d9bdf7593233e4bf50235a920b97de
Dataisgoodrepository <162981368+Dataisgoodrepository@users.noreply.github.com>
2024-08-10 19:57:47 +0530

    Add files via upload

---
+++ crime_database_questions.sql
@@ -0,0 +1,27 @@
+create database crime;
+
+use crime;
+
+-- crime_data_questions
+
+-- 1. Find the total number of crimes recorded in the CRIME table.
+
+
+-- 2. List community areas with per capita income less than 11000.
+
+-- 3. List all case numbers for crimes  involving minors?
+
+
+-- 4. List all kidnapping crimes involving a child?
+
+
+-- 5. What kinds of crimes were recorded at schools?
+
+
+-- 6. List the average safety score for each type of school.
+
+
+-- 7.  List 5 community areas with highest % of households below poverty line
+
+
+-- 8. Which community area is most crime prone?
